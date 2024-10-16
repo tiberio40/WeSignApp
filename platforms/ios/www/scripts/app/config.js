5eeae1c82df7d9b584122a05a0365f50d829b960
tiberio40 <frenando40@hotmail.com>
2024-10-16 09:02:15 -0500

    add AD

--- platforms/ios/www/scripts/app/config.js
+++ platforms/ios/www/scripts/app/config.js
@@ -16,7 +16,7 @@ var environmentConfig = EnviromentType.qa;
 var vesionAppConfig = "6.0.3";
 var serviceUrl = "";
 
-let tenant = "";
+let tenant = "8b86a65e-3c3a-4406-8ac3-19a6b5cc52bc";
 var clientId = "";
 var clientSecret = "";
 var redirect_uri = "";
@@ -34,12 +34,12 @@ switch(environmentConfig){
 }
 
 if(EnviromentType.prod == environmentConfig ){
-    clientId = "";
-    clientSecret = "";
+    clientId = "7b71c680-e574-49ff-8ba9-32681a2b7f19";
+    clientSecret = "Hvp8Q~g6VZ4HA.hBd6Oxor3CeDocjq1.IF6bTbm7";
     redirect_uri = "https%3A%2F%2Fwww.wesignapp.com%2F%23%2Foauth%2FloginCallback?state=300";
 }else{
-    clientId = "";
-    clientSecret = "";
+    clientId = "d9b8c28f-09ba-4d30-ae36-d5f26b38564b";
+    clientSecret = "wtL8Q~NY1ft-hnYXdT6dfCZfiQtx3LROtnRM5acj";
     redirect_uri = "https%3A%2F%2Fqa.www.wesignapp.com%2F%23%2Foauth%2FloginCallback?state=300";
 }
 
